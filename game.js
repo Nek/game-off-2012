bc33e3a453c53c88dcba6e1b6127ad7230364e9d
Nek <nikdudnik@gmail.com>
2012-11-16 15:04:30 +0700

    Structure the code Add comments.

--- game.js
+++ game.js
@@ -5,84 +5,128 @@ function start() {
     var fhsv = Builder.fhsv;
 
     /*
-     * Constants
+     * Init input handling system.
+     * The returned function is polymorphic and works as simple dictionary of Numbers to Booleans.
+     * Number -> Boolean -> Boolean
+     * Number -> Boolean
+     * The ability to set values is workaround to use it with Animatron player.
+     * In the end it shall be built in animatron's player loop.
      */
-    var WIDTH = 640;
-    var HEIGHT = 360;
+    var input = (function() {
+        var keyMap = {};
+        keyMap[32] = false;
+        keyMap[39] = false;
+        keyMap[37] = false;
+        return function(kc, val) {
+            if (val !== undefined) keyMap[kc] = val;
+            return keyMap[kc];
+        };
+    })();
 
-    var G = 0.8;
+    /*
+     * Create scene to hold it all together.
+     */
+    var scene = b().band([0,Number.MAX_VALUE]);
 
-    var level = b().band([0, Number.MAX_VALUE])
-            .rect([0,200],[200,20])
-            .reg([ -100, -10 ]);    
+    /*
+     * Init rendering.
+     * This step creates level and avatar objects for animatron's player.
+     * The returned function takes game world specific values and updates those objects accordingly.
+     * It would evolve with every change to the game world.
+     */
+    var render = (function(scene) {
+        var level = b().band([0, Number.MAX_VALUE])
+                .rect([0,200],[200,20])
+                .reg([ -100, -10 ]);
 
-    var avatar = b().band([0,Number.MAX_VALUE])
-            .rect([0,0],[20,20])
-            .reg([ -10, -10 ]);
+        var avatar = b().band([0,Number.MAX_VALUE])
+                .rect([0,0],[20,20])
+                .reg([ -10, -10 ]);
+        scene
+            .add(level)
+            .add(avatar);
 
-    var keyMap = {};
-    keyMap[32] = false;
-    keyMap[39] = false;
-    keyMap[37] = false;
+        return function(x,y){
+            avatar.v.xdata.pos[0] = x;
+            avatar.v.xdata.pos[1] = y;
+        };
+    })(scene);
 
-    function rect(x, y, w, h) {
-        return [
+    /*
+     * Init world.
+     * Creates game world which is a function of input and render.
+     * The contract for input is:
+     * Number -> Boolean
+     * So the game knows which key is pressed in the current game cycle.
+     * The contract for render is:
+     * Number -> Number -> undefined
+     * It takes player's position and renders it on screen.
+     * We don't care about the function's returned value.
+     */
+    var world = (function () {
+        var G = 9.8;
+
+	var player_x = 0;
+	var player_y = 0;
+        var player_vy = 0;
+        var player_vx = 0;
+        var current_time = new Date().getTime();
+
+        /*
+         *  Helper function to transform game specific values into collision detection specific representation.
+         */ 
+        var rect = function (x, y, w, h) {
+            return [
                 [x,y],
                 [x+w,y],
                 [x+w,y+h],
                 [x, y+h]
             ];
-    }
-
-    function input(kc) {
-        return keyMap[kc];
-    }
-
-    var world = (function () {
-	var player_x = 0;
-	var player_y = 0;
-        var player_vy = 0;
-        var player_vx = 0;
-        var current_time = new Date().getTime();    
-//	var map = [...];
-        var new_world = function(){
+        };
+    
+        return function(input, render){
             var time = new Date().getTime();
             var dt = time - current_time;
             current_time = time;
             player_vx = (input(39) ? 100 : 0) + (input(37) ? -100 : 0);
-            player_vy += 9.8;	
+            player_vy += G;	
             player_x += player_vx*(dt/1000);
             player_y += player_vy*(dt/1000);
             var coll_data = collide(rect(player_x, player_y, 20, 20), rect(0,200,200,20));
+            // Use collision data and input to decide what happens next.
             if (coll_data) {
-                //console.log(coll_data);
                 player_vy = input(32) ? -300 : 0;
                 player_y += coll_data.overlap;
             } else {
-                //player_x = coll_data.contact.x;
             }
-            //		...
             // Render
-            avatar.v.xdata.pos[0] = player_x;
-            avatar.v.xdata.pos[1] = player_y;
-            return new_world;
+            render(player_x, player_y);
 	};
-        return new_world(current_time);
     })();
     
-    var scene = b().band([0,Number.MAX_VALUE])
-            .add(level)
-            .add(avatar)
-            .on(C.X_KUP, function(e){
-                keyMap[e.key] = false;
-            })
-            .on(C.X_KDOWN, function(e){
-                keyMap[e.key] = true;
-            })
-            .modify(function(){
-                world = world();
-            });
+    /*
+     * This glues everything together.
+     * I don't like the use of keyboard events and closure to pass input to the world.
+     * I'd prefer to turn on, off events on a player's object so they are passed to every modifier attached.
+     * Draft:
+     * scene.registerKeyEvent('SPACE')
+     *      .modify(function(t, input){
+     *          var is_pressed = input('SPACE');
+     *          // false if it's not pressed in the current player's cycle
+     *          // true if pressed in the current player's cycle
+     *      })
+     */
+    scene
+        .on(C.X_KUP, function(e){
+            input(e.key, false);
+        })
+        .on(C.X_KDOWN, function(e){
+            input(e.key, true);
+        })
+        .modify(function(){
+            world(input, render);
+        });
 
-    var player = createPlayer('canv', {'mode':C.M_DYNAMIC, 'anim':{"bgfill": { color: "#000" },'width':WIDTH, 'height':HEIGHT}});
+    var player = createPlayer('canv', {'mode':C.M_DYNAMIC, 'anim':{"bgfill": { color: "#000" },'width':640, 'height':360}});
     player.load(scene).play();
 }
