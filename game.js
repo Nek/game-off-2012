e2b5631762573e3c0de8e45942a35264c860026a
Nek <nikdudnik@gmail.com>
2012-11-16 13:25:15 +0700

    Add animatronless game

--- game.js
+++ game.js
@@ -1,84 +1,8 @@
 function start() {
 
-    anm.M.collisions.pathDriven = true;
+
     var b = Builder._$, C = anm.C;
-
     var fhsv = Builder.fhsv;
-
-    // from underscore.js
-    var once = function(func) {
-        var ran = false;
-        var memo;
-        return function() {
-            if (ran) return memo;
-            ran = true;
-            memo = func.apply(this, arguments);
-            return memo;
-        };
-    };
-
-    var countFrames = function () {
-        if (this.currentFrame === undefined) this.currentFrame = 0;
-        else this.currentFrame += 1;
-    };
-
-    var doIfThis = function (cond, l) {
-        return function (t, d) {
-            if (cond.call(this)) {
-                return l.call(this, t, d);
-            }
-        };
-    };
-
-    var doIf = function (cond, l) {
-        return function (t, d) {
-            if (cond()) {
-                return l.call(this, t, d);
-            }
-        };
-    };
-
-    var currentFrameEq = function (i) {
-        return function () {
-            return this.currentFrame === i;
-        };
-    };
-
-    var currentFrameGt = function (i) {
-        return function () {
-            return this.currentFrame > i;
-        };
-    };
-
-    var currentFrameLt = function (i) {
-        return function () {
-            return this.currentFrame < i;
-        };
-    };
-
-    var onFrame = function (i, l) {
-        return doIfThis(currentFrameEq(i), l);
-    };
-
-    var afterFrame = function (i, l) {
-        return doIfThis(currentFrameGt(i), l);
-    };
-
-/*
- * state:
- * world = menu | game_play | game_pause | game_over | scores_table
- * game_play = player, scores
- * game_pause = player, scores
- * game_over = scores
- * scores_table = [scores]
- * menu = selected_item
- * player = speed, position, run | jump
- * 
- * 
- * loop:
- * 
- * 
- */
 
     /*
      * Constants
@@ -88,16 +12,6 @@ function start() {
 
     var G = 0.8;
 
-    anm.M[C.MOD_COLLISIONS].predictSpan = 0.06;
-
-    var world = {
-        speed : 3,
-        jump: 0,
-        py : 0,
-        px : 100,
-        on_earth : false
-    };
-
     var level = b().band([0, Number.MAX_VALUE])
             .rect([0,0],[5200,100]);
     
@@ -105,35 +19,9 @@ function start() {
     var avatar = b().band([0,Number.MAX_VALUE])
     .rect([0,0],[70,70]);
 
-    function loop(w, input, observ) {
-
-       // if (!level.v.intersects(avatar.v)) {
-           
-       // } else if (input.toString() === 'false,true') w.jump = -15; else w.jump = 0; 
-
-        if (observ !== null ) {
-            //avatar.v.xdata.pos[0] = observ.lx;
-            //avatar.v.xdata.pos[1] = observ.ly;
-            observ = null;
-        } else {
-            w.jump += G;
-            w.py += w.jump;
-            avatar.v.xdata.pos[0] = w.speed*10;
-            avatar.v.xdata.pos[1] = w.py;
-        }
-
-       
-        w.px += w.speed;
-        w.speed += 0.1;
-
-        level.v.xdata.pos[0] = -w.px;
-        level.v.xdata.pos[1] = HEIGHT;
-    }
-
-    
+    //(input.toString() === 'false,true')
 
     var input = [false,false];
-    var observations = null;
 
     var scene = b().band([0,Number.MAX_VALUE])
             .add(level)
@@ -155,6 +43,75 @@ function start() {
             })
             .modify(function(){loop(world, input, observations);});
 
-    var player = createPlayer('gameCanvas', {'mode':C.M_DYNAMIC, 'anim':{"bgfill": { color: "#000" },'width':WIDTH, 'height':HEIGHT}});
-    player.load(scene).play();
+    //var player = createPlayer('gameCanvas', {'mode':C.M_DYNAMIC, 'anim':{"bgfill": { color: "#000" },'width':WIDTH, 'height':HEIGHT}});
+    //player.load(scene).play();
+
+    var ctx = canv.getContext("2d");
+
+    var keyMap = {};
+    keyMap[32] = false;
+    keyMap[39] = false;
+    keyMap[37] = false;
+
+    window.onkeydown = function(e) {
+        console.log(e.keyCode);
+        keyMap[e.keyCode] = true;
+    };
+
+    window.onkeyup = function(e) {
+        keyMap[e.keyCode] = false;
+    };
+
+    function rect(x, y, w, h) {
+        return [
+                [x,y],
+                [x+w,y],
+                [x+w,y+h],
+                [x, y+h]
+            ];
+    }
+
+    function input2(kc) {
+        //if (keyMap[kc] === undefined) return false;
+        return keyMap[kc];
+    }
+
+    function create_world() {
+	    var player_x = 0;
+	    var player_y = 0;
+        var player_vy = 0;
+        var player_vx = 0;
+        var current_time = new Date().getTime();    
+//	var map = [...];
+        var new_world = function(time){
+            var dt = time - current_time;
+            current_time = new Date().getTime();
+            player_vx = (input2(39) ? 100 : 0) + (input2(37) ? -100 : 0);
+            player_vy += 9.8;	
+            player_x += player_vx*(dt/1000);
+            player_y += player_vy*(dt/1000);
+            var coll_data = collide(rect(player_x, player_y, 20, 20), rect(0,200,200,20));
+            if (coll_data) {
+                //console.log(coll_data);
+                player_vy = input2(32) ? -300 : 0;
+                player_y += coll_data.overlap;
+            } else {
+                //player_x = coll_data.contact.x;
+            }
+//		...
+            render([[player_x, player_y,20,20],[0,200,200,20]]);
+            webkitRequestAnimationFrame(new_world);
+	    };
+
+        new_world(current_time);
+    }
+
+    function render(objs) {
+        ctx.clearRect(0,0,1000,1000);
+        objs.forEach(function(obj) {
+            ctx.fillRect.apply(ctx, obj);            
+        });
+    }
+    
+    create_world();
 }
